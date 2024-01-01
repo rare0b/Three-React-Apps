70e25bff8ab944c142c6eabedde995ff268eddf2
rare0b <rare0b0110@gmail.com>
2024-01-01 22:36:09 +0900

    feat: ポケモン図鑑の実装

---
+++ pokemon-app/src/App.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [prevURL, setPrevURL] = useState("");
+  const [nextURL, setNextURL] = useState("");
+
+  useEffect(() => {
+    fetchPokemonData(initialURL);
+  }, []);
+
+  const fetchPokemonData = async (url) => {
+    setLoading(true);
+
+    //全てのポケモンデータを取得
+    let res = await getAllPokemon(url);
+    //各ポケモンの詳細なデータを取得
+    loadPokemon(res.results);
+
+    setPrevURL(res.previous ?? "");
+    setNextURL(res.next ?? "");
+
+    setLoading(false);
+  };
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  const handlePrevPage = async () => {
+    fetchPokemonData(prevURL);
+  };
+
+  const handleNextPage = async () => {
+    fetchPokemonData(nextURL);
+  };
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <Navbar />
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+          <div className="btn">
+            {prevURL ? <button onClick={handlePrevPage}>前へ</button> : <></>}
+            {nextURL ? <button onClick={handleNextPage}>次へ</button> : <></>}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemon-app/src/components/Card/Card.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Card.css";
+
+const Card = ({ pokemon }) => {
+  return (
+    <div className="card">
+      <div className="cardImg">
+        <img
+          alt={`${pokemon.name}'s img`}
+          src={pokemon.sprites.front_default}
+        ></img>
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        {pokemon.types.map((type) => {
+          return (
+            <div key={type.type.name}>
+              <span className="typeName">{type.type.name}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">weight: {pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">height: {pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">ability: {pokemon.abilities[0].ability.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
