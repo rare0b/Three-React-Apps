b6a39b09bea94689e9ca00a936ea4063c14ebf60
rare0b <rare0b0110@gmail.com>
2024-01-04 02:11:29 +0900

    feat: Navbarの実装

--- blog-with-react-and-firebase/src/components/Navbar.js
+++ blog-with-react-and-firebase/src/components/Navbar.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightToBracket,
+  faFilePen,
+  faHouse,
+} from "@fortawesome/free-solid-svg-icons";
 
-const Navbar = () => {
+const Navbar = ({ isAuth }) => {
   return (
     <nav>
-      <Link to="/">ホーム</Link>
-      <Link to="/createpost">記事投稿</Link>
-      <Link to="/login">ログイン</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon>
+        ホーム
+      </Link>
+      <Link to="/createpost">
+        <FontAwesomeIcon icon={faFilePen}></FontAwesomeIcon>
+        記事投稿
+      </Link>
+      {!isAuth ? (
+        <Link to="/login">
+          <FontAwesomeIcon icon={faArrowRightToBracket}></FontAwesomeIcon>
+          ログイン
+        </Link>
+      ) : (
+        <Link to="/logout">
+          <FontAwesomeIcon icon={faArrowRightToBracket}></FontAwesomeIcon>
+          ログアウト
+        </Link>
+      )}
     </nav>
   );
 };
