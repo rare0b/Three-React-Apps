908036a5e1656895e95bf8c531c7126aae553236
rare0b <rare0b0110@gmail.com>
2024-01-17 01:23:17 +0900

    feat: ノートの一覧表示・追加・削除

--- noteapp-with-react/src/App.jsx
+++ noteapp-with-react/src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 import Main from "./components/Main/Main";
 import Sidebar from "./components/Sidebar/Sidebar";
+import uuid from "react-uuid";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log("ノートが追加されました");
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const onDeleteNote = (id) => {
+    const filteredNotes = notes.filter((note) => note.id !== id);
+    setNotes(filteredNotes);
+  };
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        notes={notes}
+      />
       <Main />
     </div>
   );
